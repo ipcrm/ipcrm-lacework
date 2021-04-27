447fa5f5b4b0374274b233f86db61005ad61c1bc
ipcrm <mcadorette@gmail.com>
2021-04-26 20:51:19 -0400

    cleanup acceptance test

--- spec/acceptance/lacework_spec.rb
+++ spec/acceptance/lacework_spec.rb
@@ -2,31 +2,21 @@ require 'spec_helper_acceptance'
 
 pp_basic = <<-PUPPETCODE
   class {'lacework':
-    # access_token => "no",
-    # config_tags => {
-      # test => "b",
-      # testa => "c",
-      # testb => "d",
-      # testc => "e",
-    # }
-
-
-          access_token              => 'no',
-          proxyurl                  => 'proxy.host',
-          config_tags               => {a => 'b', b => 'c'},
-          perfmode                  => 'lite',
-          cpulimit                  => '500m',
-          memlimit                  => '750M',
-          auto_upgrade              => 'disabled',
-          container_engine_endpoint => 'tcp://0.0.0.0:2375',
-          cmdlinefilter_allow       => "/bin/echo,/bin/ls",
-          cmdlinefilter_disallow    => "/bin/java,/bin/node",
-          fim_filepath              => ["/home/test/.ssh"],
-          fim_fileignore            => ["/etc/fstab"],
-          fim_runat                 => "23:50",
-          fim_mode                  => "disable",
-          fim_noatime               => true,
-
+    access_token              => 'no',
+    proxyurl                  => 'proxy.host',
+    config_tags               => {a => 'b', b => 'c'},
+    perfmode                  => 'lite',
+    cpulimit                  => '500m',
+    memlimit                  => '750M',
+    auto_upgrade              => 'disabled',
+    container_engine_endpoint => 'tcp://0.0.0.0:2375',
+    cmdlinefilter_allow       => "/bin/echo,/bin/ls",
+    cmdlinefilter_disallow    => "/bin/java,/bin/node",
+    fim_filepath              => ["/home/test/.ssh"],
+    fim_fileignore            => ["/etc/fstab"],
+    fim_runat                 => "23:50",
+    fim_mode                  => "disable",
+    fim_noatime               => true,
   }
 PUPPETCODE
 
